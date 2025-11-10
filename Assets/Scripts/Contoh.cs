9c3e6887a9cf643c73cbf82cec615183cb1f2db2
apriandirian93 <apriandi.rian93@gmail.com>
2025-11-10 14:57:27 +0700

    Pelatihan1

--- Assets/Scripts/Contoh.cs
+++ Assets/Scripts/Contoh.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class Contoh : MonoBehaviour
 {
+    int number1 = 10;
+    int number2 = 7;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello World");
+        int number3 = number1;
+        number1 = number2;
+        number2 = number3;
+        Debug.Log(number1);
+        Debug.Log(number2);
     }
 
     // Update is called once per frame
