fdf0d557767369cff009989e86509dcaae3d8760
apriandirian93 <apriandi.rian93@gmail.com>
2025-11-11 09:07:23 +0700

    Update ContohLoop.cs

--- Assets/Scripts/ContohLoop.cs
+++ Assets/Scripts/ContohLoop.cs
@@ -5,11 +5,15 @@ public class ContohLoop : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int nyawa = 10;
-        while (nyawa > 0)
+        //int nyawa = 10;
+        // while (nyawa > 0)
+        // {
+        //     nyawa = nyawa - 1;
+        //     Debug.Log(nyawa);
+        // }
+        for(int nyawa = 10; nyawa > 0; nyawa--)
         {
-            nyawa = nyawa - 1;
-            Debug.Log(nyawa);
+            Debug.Log("Sisa nyawa: " + nyawa);
         }
         Debug.Log("Game Over");
     }
