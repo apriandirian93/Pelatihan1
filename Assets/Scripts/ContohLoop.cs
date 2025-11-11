997d4e27e76e9d08f4549fb2e6e981e319946b0a
apriandirian93 <apriandi.rian93@gmail.com>
2025-11-11 09:41:52 +0700

    Update ContohLoop.cs

--- Assets/Scripts/ContohLoop.cs
+++ Assets/Scripts/ContohLoop.cs
@@ -11,12 +11,20 @@ public class ContohLoop : MonoBehaviour
         //     nyawa = nyawa - 1;
         //     Debug.Log(nyawa);
         // }
-        
-        for(int nyawa = 10; nyawa > 0; nyawa--)
+
+        // for(int nyawa = 10; nyawa > 0; nyawa--)
+        // {
+        //     Debug.Log("Sisa nyawa: " + nyawa);
+        // }
+        // Debug.Log("Game Over");
+
+        int faktorial = 4;
+        int hasil = 1;
+        for (int i = faktorial; i > 0; i--)
         {
-            Debug.Log("Sisa nyawa: " + nyawa);
+            hasil = hasil * i;
         }
-        Debug.Log("Game Over");
+        Debug.Log(hasil);
     }
 
     // Update is called once per frame
