911bb293eeb82d4e43a4947b109593b818a36948
apriandirian93 <apriandi.rian93@gmail.com>
2025-11-12 11:30:12 +0700

    Belajar UI

---
+++ Assets/Scripts/12 November 2025/AksiTombol.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AksiTombol : MonoBehaviour
+{
+    // Sprite adalah asset gambar yang ingin dimasukkan ke object image
+    public Sprite assetGambar;
+    // Image adalah component dari object image yang ada di dalam scene
+    public Image image;
+    public TMP_Text objectText;
+    public void Aksi()
+    {
+        // mengubah gambar
+        image.sprite = assetGambar;
+        // mengubah teks
+        objectText.text = "RAM";
+        Debug.Log("Tombol ditekan");
+    }
+}
