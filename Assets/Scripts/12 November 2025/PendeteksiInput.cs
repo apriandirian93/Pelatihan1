8146174deb99aa604e2d431a7c6bee4cc592bf65
apriandirian93 <apriandi.rian93@gmail.com>
2025-11-12 09:57:41 +0700

    Update PendeteksiInput.cs

--- Assets/Scripts/12 November 2025/PendeteksiInput.cs
+++ Assets/Scripts/12 November 2025/PendeteksiInput.cs
@@ -1,21 +1,17 @@
+using System;
 using UnityEngine;
 
 public class PendeteksiInput : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-    // Input.GetKey untuk tekan dan tahan
-    // Input.GetKeyUp ketika dilepas tombolnya
-    public Score objectScore;
+    // public Score objectScore;
+    public Action inputEvent;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            objectScore.AddScore();
+            // objectScore.AddScore();
+            inputEvent.Invoke();
         }
     }
 }
